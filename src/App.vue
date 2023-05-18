b47ab7f798d17a82deae8dbb09a76fc865cdb1ed
StefanoGalandrini <stefe68@gmail.com>
2023-05-18 18:48:58 +0200

    Add top Navbar

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
 </template>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;500;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Encode+Sans+Condensed:wght@600&family=Open+Sans:wght@300;400;600&display=swap");
 
 *,
 *::before,
@@ -33,6 +33,7 @@ export default {
 }
 
 :root {
-	font-family: "Noto Sans", sans-serif;
+	font-family: "Encode Sans Condensed", sans-serif;
+	font-family: "Open Sans", sans-serif;
 }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,20 +4,77 @@ export default {};
 
 <template>
 	<div class="container">
-		<nav>Nav</nav>
+		<div class="top-bar">
+			<img src="../assets/img/dc-logo.png" alt="Logo DC" />
+			<nav>
+				<ul class="menu">
+					<li><a class="item" href="#">characters</a></li>
+					<li><a class="item active" href="#">comics</a></li>
+					<li><a class="item" href="#">movies</a></li>
+					<li><a class="item" href="#">TV</a></li>
+					<li><a class="item" href="#">games</a></li>
+					<li><a class="item" href="#">collectibles</a></li>
+					<li><a class="item" href="#">videos</a></li>
+					<li><a class="item" href="#">fans</a></li>
+					<li><a class="item" href="#">news</a></li>
+					<li><a class="item" href="#">shop</a></li>
+				</ul>
+			</nav>
+		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
 $color-darkgrey: #303030;
+$color-blue: #0282f9;
 .container {
 	height: 13vh;
 	background-color: white;
 	color: $color-darkgrey;
 }
 
-nav {
+.top-bar {
+	height: 100%;
 	max-width: 70vw;
 	margin-inline: auto;
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+
+	img {
+		width: 80px;
+		aspect-ratio: 1 / 1;
+		object-fit: cover;
+		margin-right: auto;
+	}
+}
+
+nav {
+	height: 100%;
+}
+.menu {
+	height: 100%;
+	list-style-type: none;
+	display: flex;
+}
+.item {
+	display: flex;
+	height: 100%;
+	align-items: center;
+	margin-left: 1.5rem;
+	font-family: "Encode Sans Condensed";
+	text-decoration: none;
+	text-transform: uppercase;
+	border-bottom: 5px solid rgba(255, 255, 255, 0);
+	color: $color-darkgrey;
+
+	&:hover {
+		color: $color-blue;
+	}
+}
+
+.active {
+	color: $color-blue;
+	border-bottom: 5px solid $color-blue;
 }
 </style>
