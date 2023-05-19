89700b7570fe77a68a95d4b6104fa14da0b7879a
StefanoGalandrini <stefe68@gmail.com>
2023-05-19 14:09:04 +0200

    Add dynamic menu and "active" class on click

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,82 @@
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			menuItems: [
+				{
+					item: "characters",
+					isActive: false,
+				},
+				{
+					item: "comics",
+					isActive: false,
+				},
+				{
+					item: "movies",
+					isActive: false,
+				},
+				{
+					item: "tv",
+					isActive: false,
+				},
+				{
+					item: "games",
+					isActive: false,
+				},
+				{
+					item: "collectibles",
+					isActive: false,
+				},
+				{
+					item: "videos",
+					isActive: false,
+				},
+				{
+					item: "fans",
+					isActive: false,
+				},
+				{
+					item: "news",
+					isActive: false,
+				},
+				{
+					item: "shop",
+					isActive: false,
+				},
+			],
+		};
+	},
+	methods: {
+		toggleActive(item) {
+			for (let i = 0; i < this.menuItems.length; i++) {
+				this.menuItems[i].isActive = false;
+			}
+			item.isActive = !item.isActive;
+		},
+	},
+};
 </script>
 
 <template>
 	<div class="container">
 		<div class="top-bar">
-			<img src="../assets/img/dc-logo.png" alt="Logo DC" />
+			<img
+				src="../assets/img/dc-logo.png"
+				alt="Logo DC" />
 			<nav>
 				<ul class="menu">
+					<li v-for="menuItem in menuItems">
+						<a
+							href="#"
+							class="item"
+							:class="{active: menuItem.isActive}"
+							@click="toggleActive(menuItem)">
+							{{ menuItem.item }}
+						</a>
+					</li>
+				</ul>
+
+				<!-- <ul class="menu">
 					<li><a class="item" href="#">characters</a></li>
 					<li><a class="item active" href="#">comics</a></li>
 					<li><a class="item" href="#">movies</a></li>
@@ -18,7 +87,7 @@ export default {};
 					<li><a class="item" href="#">fans</a></li>
 					<li><a class="item" href="#">news</a></li>
 					<li><a class="item" href="#">shop</a></li>
-				</ul>
+				</ul> -->
 			</nav>
 		</div>
 	</div>
